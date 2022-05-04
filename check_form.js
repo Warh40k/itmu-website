ccd0d2fe9a1ff5c5da7d1ede0abcd7f180390a51
Nikita <nicita.zinkevitch@yandex.ru>
2022-05-04 21:20:30 +0300

    end form add js

---
+++ check_form.js
@@ -0,0 +1,8 @@
+function checkIt(){
+    let arr = new Array();
+	var t0=document.getElementById(' fio ').value;
+    
+	if (t0 == "") {
+		alert("Вы не указали свое имя!"); 
+		return false;            }
+return true;}
