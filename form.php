2b3ee75e77b865a67aa1d0f5f07b333e23c8246b
Nikita <nicita.zinkevitch@yandex.ru>
2022-05-08 09:22:44 +0000

    succesful insertion in db

--- form.php
+++ form.php
@@ -1,11 +1,10 @@
 <?php 
-echo $_POST['nraitsa'];
 $conn = mysqli_connect('localhost', 'nikita', '252800nz', 'alien') or die('Connection failed: ' . $conn->connect_error);
 echo 'Success connection<br>';
 
 print_r($_POST) . "<br><br>";
-foreach($_POST as $value) {
-    echo $value;
-}
-
+$stmt=$conn->prepare("INSERT INTO survey (fname,lname) VALUES(?,?)");
+$stmt->bind_param('ss',$_POST['firstname'],$_POST['surname']);
+$stmt->execute();
+echo '<p>Success insertion</p>';
 ?>
