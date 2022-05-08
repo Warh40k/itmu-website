37cb0cdd1e393e8eff69fd8df02640cc16623f30
Nikita <nicita.zinkevitch@yandex.ru>
2022-05-08 10:35:53 +0000

    success full insertion

--- form.php
+++ form.php
@@ -4,12 +4,14 @@ echo 'Success connection<br>';
 
 print_r($_POST) . "<br><br>";
 
-$stmt=$conn->prepare("INSERT INTO survey (fname,lname, gender, country, age, service_source, tariff, reasons) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+//$stmt=$conn->prepare("INSERT INTO survey (fname,lname, gender, country, age, service_source, tariff, reasons) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 $reasons = implode($_POST['reasons']);
+$function = implode($_POST['function']);
+$characteristics = implode($_POST['characteristics']);
 echo '<br>' . $reasons;
-$stmt->bind_param('ssssisss', $_POST['firstname'],$_POST['surname'],$_POST['gender'],$_POST['country'],$_POST['age'],$_POST['source'],$_POST['tariff_choice'], $reasons);
-//$stmt=$conn->prepare("INSERT INTO survey (fname,lname, gender, country, age, service_source, tariff, reasons, satisfaction, recommend, functions, design, characteristics, max_price, min_price, advert) VALUES(?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-//$stmt->bind_param('ssssisssiisisiis',$_POST['firstname'],$_POST['surname'],$_POST['gender'],$_POST['country'],$_POST['age'],$_POST['source'],$_POST['tariff_choice'],$_POST['reasons'],$_POST['nraitsa'],$_POST['recommend'],$_POST['function'],$_POST['design'],$_POST['characteristics'],$_POST['max_price'],$_POST['min_price'],$_POST['advert']);
+//$stmt->bind_param('ssssisss', $_POST['firstname'],$_POST['surname'],$_POST['gender'],$_POST['country'],$_POST['age'],$_POST['source'],$_POST['tariff_choice'], $reasons);
+$stmt=$conn->prepare("INSERT INTO survey (fname,lname, gender, country, age, service_source, tariff, reasons, satisfaction, recommend, functions, design, characteristics, max_price, min_price, advert) VALUES(?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param('ssssisssiisisiis',$_POST['firstname'],$_POST['surname'],$_POST['gender'],$_POST['country'],$_POST['age'],$_POST['source'],$_POST['tariff_choice'], $reasons,$_POST['nraitsa'],$_POST['recommend'], $function,$_POST['design'], $characteristics,$_POST['max_price'],$_POST['min_price'],$_POST['advert']);
 $stmt->execute();
 echo '<p>Success insertion</p>';
 ?>
