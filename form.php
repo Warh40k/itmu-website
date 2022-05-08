0fb26e59ccdd26a651c13f0da3e267ec7e67dc2a
Nikita <nicita.zinkevitch@yandex.ru>
2022-05-08 10:20:56 +0000

    change values of checkbox & success insertion array

--- form.php
+++ form.php
@@ -4,8 +4,10 @@ echo 'Success connection<br>';
 
 print_r($_POST) . "<br><br>";
 
-$stmt=$conn->prepare("INSERT INTO survey (fname,lname, gender, country, age, service_source, tariff) VALUES (?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param('ssssiss', $_POST['firstname'],$_POST['surname'],$_POST['gender'],$_POST['country'],$_POST['age'],$_POST['source'],$_POST['tariff_choice']);
+$stmt=$conn->prepare("INSERT INTO survey (fname,lname, gender, country, age, service_source, tariff, reasons) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+$reasons = implode($_POST['reasons']);
+echo '<br>' . $reasons;
+$stmt->bind_param('ssssisss', $_POST['firstname'],$_POST['surname'],$_POST['gender'],$_POST['country'],$_POST['age'],$_POST['source'],$_POST['tariff_choice'], $reasons);
 //$stmt=$conn->prepare("INSERT INTO survey (fname,lname, gender, country, age, service_source, tariff, reasons, satisfaction, recommend, functions, design, characteristics, max_price, min_price, advert) VALUES(?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 //$stmt->bind_param('ssssisssiisisiis',$_POST['firstname'],$_POST['surname'],$_POST['gender'],$_POST['country'],$_POST['age'],$_POST['source'],$_POST['tariff_choice'],$_POST['reasons'],$_POST['nraitsa'],$_POST['recommend'],$_POST['function'],$_POST['design'],$_POST['characteristics'],$_POST['max_price'],$_POST['min_price'],$_POST['advert']);
 $stmt->execute();
